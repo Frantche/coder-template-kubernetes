aca60cf3eeba664b056d7977e817916752dfd011
Frantche <fklieber@gmail.com>
2026-01-29 01:05:01 +0100

    fix: update ghcr.io/frantche/coder-full docker tag to v0.5.70 (#127)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- template-kubernetes/main.tf
+++ template-kubernetes/main.tf
@@ -251,7 +251,7 @@ resource "kubernetes_pod" "main" {
     container {
       name = "dev"
       # renovate: datasource=docker depName=ghcr.io/frantche/coder-full versioning=docker
-      image             = "ghcr.io/frantche/coder-full:0.5.69"
+      image             = "ghcr.io/frantche/coder-full:0.5.70"
       image_pull_policy = "Always"
       command           = ["sh", "-c", coder_agent.main.init_script]
       security_context {
